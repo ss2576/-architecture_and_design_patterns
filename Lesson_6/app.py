4cc640510f1714c94d328a0a00753e24991fcceb
ss2576 <ss2576@mail.ru>
2020-10-01 15:57:27 +0300

    Урок_6

---
+++ Lesson_6/app.py
@@ -0,0 +1,33 @@
+from framework.main import Application, DebugApplication, FakeApplication, Render
+from urls import routes, routes_post
+from views import fronts
+from models import site
+from logging_mod import debug
+
+application = Application(routes, routes_post, fronts)
+# application = DebugApplication(routes, routes_post, fronts)
+# application = FakeApplication(routes, routes_post, fronts)
+render = Render()
+
+
+@application.add_route('/copy-course/')
+@debug
+def copy_course(request):
+	request_params = request['request_params']
+	name = request_params['name']
+	old_course = site.get_course(name)
+	if old_course:
+		new_name = f'copy_{name}'
+		new_course = old_course.clone()
+		new_course.name = new_name
+		site.courses.append(new_course)
+	categories = site.categories
+	courses = site.courses
+	data = {
+		'title': 'Созданы курсы',
+		'categories': categories,
+		'courses': courses,
+	}
+	content = render('create_course.html', object_list=data)
+	code = '202 Accepted'
+	return code, content
